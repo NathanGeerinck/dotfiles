9ffd5d918161b12aeb4df62e73a529dea7c59b55
Nathan Geerinck <nathan.geerinck@gmail.com>
2021-05-07 13:31:44 +0200

    Added some aliasses

--- aliases.zsh
+++ aliases.zsh
@@ -6,3 +6,11 @@ alias code='open -a "/Applications/Visual Studio Code.app" "`pwd`"'
 
 # Laravel
 alias pa='php artisan'
+
+# Git
+alias pull="git pull"
+alias push="git push"
+alias commit="git add . && git commit -m 'wip'"
+
+# Restart touch bar
+alias touchbar="killall ControlStrip"
