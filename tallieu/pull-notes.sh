e555b2794df3459bc0a19339cfa390ef6df78144
Nathan Geerinck <nathan@intilli.be>
2024-10-22 16:38:49 +0200

    Refactor tnt-sync-notes & tnt-pull-notes commands

--- tallieu/pull-notes.sh
+++ tallieu/pull-notes.sh
@@ -1,2 +1,7 @@
+PREV_DIR=$(pwd)
+
 cd ~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Intilli/Tallieu\ \&\ Tallieu
+
 git pull
+
+cd "$PREV_DIR"
--- tallieu/sync-notes.sh
+++ tallieu/sync-notes.sh
@@ -1,5 +1,13 @@
+PREV_DIR=$(pwd)
+
 cd ~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Intilli/Tallieu\ \&\ Tallieu
+
 git pull
+
 git add .
+
 git commit -m "Add notes"
+
 git push
+
+cd "$PREV_DIR"
