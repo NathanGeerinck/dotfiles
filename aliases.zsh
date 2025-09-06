32127587617c5f28196a56df1e1402e384244930
Nathan Geerinck <nathan@intilli.be>
2025-09-06 16:32:10 +0200

    Remove VS code alias

--- aliases.zsh
+++ aliases.zsh
@@ -4,9 +4,6 @@ alias reload=". ~/.zshrc && echo 'ZSH config reloaded from ~/.zshrc'"
 # PHPStorm
 alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 
-# VSCode
-alias code='open -a "/Applications/Visual Studio Code.app" "`pwd`"'
-
 # PHP
 alias switch-php84="brew unlink php && brew link --overwrite --force php@8.4"
 alias switch-php83="brew unlink php && brew link --overwrite --force php@8.3"
