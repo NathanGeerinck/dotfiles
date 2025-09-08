1a92d83d123e5f01f5a66d3215452cff63e2f62a
Nathan Geerinck <nathan@intilli.be>
2025-09-08 11:35:06 +0200

    Move T&T functionality to separated folder

--- aliases.zsh
+++ aliases.zsh
@@ -29,35 +29,7 @@ alias dcu="docker compose up"
 alias dps="docker ps"
 
 # Tallieu & Tallieu
-tnt-help() {
-  echo -e "$fg_bold[magenta]TNT (Tallieu & Tallieu) Commands:$reset_color"
-  echo -e "  tnt-open <project>           Open a project in PHPStorm$reset_color"
-  echo -e "  tnt-clone <repo> [-gh]       Clone a repository from Bitbucket (default) or Github with -gh flag$reset_color"
-  echo -e "  tnt-pull-notes               Pull Obsidian notes$reset_color"
-  echo -e "  tnt-sync-notes [message]     Sync Obsidian notes with optional commit message$reset_color"
-  echo -e "  tnt-opencode [args]          Run opencode with T&T credentials, passes all args to opencode$reset_color"
-  echo -e "  tnt-help                     Show this help message$reset_color"
-}
-
-tnt-open() {
-  source ~/.dotfiles/tallieu/open.sh "$@"
-}
-
-tnt-clone() {
-  source ~/.dotfiles/tallieu/clone.sh "$@"
-}
-
-tnt-pull-notes() {
-  source ~/.dotfiles/tallieu/pull-notes.sh "$@"
-}
-
-tnt-sync-notes() {
-  source ~/.dotfiles/tallieu/sync-notes.sh "$@"
-}
-
-tnt-opencode() {
-  source ~/.dotfiles/tallieu/opencode.sh "$@"
-}
+source ~/.dotfiles/tallieu/tnt.zsh
 
 # Empty the Trash on all mounted volumes and the main HDD
 # Also, clear Apple’s System Logs to improve shell startup speed
